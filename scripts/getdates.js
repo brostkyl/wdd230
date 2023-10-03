ef69f6bda78a22e8ad76cb0b43200771036391cb
brostkyl <bro19089@byui.edu>
2023-10-02 22:46:50 -0600

    Changes made to home and siteplan created

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,19 @@
 let today1 = new Date();
 document.querySelector("#currentyear").textContent=today1.getFullYear()
 document.querySelector("#lastmodified").textContent=document.lastModified
+//Hamburger Menu code. 
+const hidebutton = document.querySelector(".menu-hide");
+const showbutton = document.querySelector(".menu-show");
+const navmenu = document.querySelector("#nav-menu"); 
+
+hidebutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');
+});
+
+showbutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');        
+});
