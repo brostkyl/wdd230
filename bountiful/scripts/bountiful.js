c65aba75acb7231642a9b3a7d02dfdc77cba728b
brostkyl <bro19089@byui.edu>
2023-12-13 10:32:24 -0600

    java

--- bountiful/scripts/bountiful.js
+++ bountiful/scripts/bountiful.js
@@ -1,4 +1,3 @@
-//Last Modified
 let today1 = new Date();
 document.querySelector("#currentyear").textContent=today1.getFullYear()
 document.querySelector("#lastmodified").textContent=document.lastModified
