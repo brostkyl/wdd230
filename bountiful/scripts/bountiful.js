153029e63e29312b74566a70384ae0586b10ff88
brostkyl <bro19089@byui.edu>
2023-12-13 10:18:59 -0600

    updates to bountiful site

---
+++ bountiful/scripts/bountiful.js
@@ -0,0 +1,20 @@
+//Last Modified
+let today1 = new Date();
+document.querySelector("#currentyear").textContent=today1.getFullYear()
+document.querySelector("#lastmodified").textContent=document.lastModified
+//Hamburger Menu code. 
+const hidebutton = document.querySelector(".menu-hide");
+const showbutton = document.querySelector(".menu-show");
+const navmenu = document.querySelector("#nav-menu"); 
+
+hidebutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');
+});
+
+showbutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');        
+});
